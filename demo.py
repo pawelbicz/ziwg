28da37c97aec7c689b8677b05fe3b749a9c0bf09
server <server@server.pl>
2017-01-24 17:03:40 -0500

    missing files in previous commit

--- demo.py
+++ demo.py
@@ -20,16 +20,107 @@ app.config['MYSQL_HOST'] = config.get('KEY', 'host')
 mysql = MySQL(app)
 app.config['JSONIFY_PRETTYPRINT_REGULAR']
 
+@app.route('/test1', methods=['POST'])
+def test1():
+    content = request.json
+    addUserToDatabase(str(content['login']),str(content['password']))
+    return "Sru"
 
-@app.route('/odpytaj')
-def odpytaj():
+@app.route('/test2', methods=['POST'])
+def test2():
+    content = request.json
+    removeUserToDatabase(str(content['login']))
+    return "Sru"
+
+
+@app.route('/test3', methods=['POST'])
+def test3():
+    content = request.json
+    checkIfUserIsInDatabase(str(content['login']),str(content['password']))
+    return "Sru"
+
+@app.route('/test4', methods=['POST'])
+def test4():
+    showUsersInDatabase()
+    return "Sru"
+
+
+def addUserToDatabase(login,password):
+    print 'addUserToData'
     conn = mysql.connection
     cursor = conn.cursor()
-    cursor.execute("SELECT * FROM users LIMIT 1")    
+    ifLoginExists = 0
+    cursor.execute("SELECT * FROM users")
     rows = cursor.fetchall()
     for row in rows:
+        if login == row[1] and password == row[2]:
+            ifLoginExists = 1
+    if ifLoginExists == 1:
+        print "Debug: Nie dodano uzytkownika poniewaz jest juz w bazie"
+        return "Nie dodano uzytkownika poniewaz jest juz w bazie"
+    else:
+        privileges = 0
+        mysqlCmd = "INSERT INTO `users` (`user_username`,`user_password`,`user_privileges`) VALUES ("+"'"+str(login)+"','"+str(password)+"','"+str(privileges)+"');"
+        print mysqlCmd
+        cursor.execute(mysqlCmd)
+        cursor.execute("SELECT * FROM users")
+        rows = cursor.fetchall()
+        for row in rows:
+            print row
+        return "Dodano uzytkownika do bazy danych"
+
+def removeUserFromDatabase(login):
+    conn = mysql.connection
+    cursor = conn.cursor()
+    ifLoginExists = 0
+    cursor.execute("SELECT * FROM users")
+    rows = cursor.fetchall()
+    for row in rows:
+        if login == row[1]:
+            ifLoginExists = 1
+    if ifLoginExists == 0:
+        return "Nie usunieto  uzytkownika poniewaz nie ma go w bazie"
+    else:
+       mysqlCmd = "DELETE FROM `users` WHERE user_username = "+"'"+str(login)+"'"
+       cursor.execute(mysqlCmd)
+       cursor.execute("SELECT * FROM users")
+       rows = cursor.fetchall()
+       for row in rows:
+           print row
+       return "Uzytkownik o podanym loginie zostal usuniety z bazy"
+
+def checkIfUserIsInDatabase(login,password):
+    conn = mysql.connection
+    cursor = conn.cursor()
+    mysqlCmd = "SELECT * FROM users"
+    ifLoginCorrect = False
+    cursor.execute(mysqlCmd)
+    rows = cursor.fetchall()
+    for row in rows:
+        if login == row[1] and password == row[2]:
+            ifLoginCorrect = True
+    if ifLoginCorrect == True:
+        print "Debug: uzytkownik jest"
+        return True
+    else:
+        print "Debug: uzytkownika nie ma"
+        abort(401)
+        return False
+
+def listRaceTableFromDatabase():
+    return True
+
+def showUsersInDatabase():
+    conn = mysql.connection
+    cursor = conn.cursor()
+    listOfUsers = []
+    cursor.execute("SELECT * FROM `users`")
+    rows = cursor.fetchall()
+    for row in rows:
+        listOfUsers.append(row)
         print row
-    return str(rows)
+    print listOfUsers
+    return listOfUsers
 
 
 @app.route('/addlogin', methods=['POST'])
@@ -37,17 +128,14 @@ def addlogin():
     content = request.json
     conn = mysql.connection
     cursor = conn.cursor()
-#    msg = 'SELECT * FROM users LIMIT 1'
-    msg = "INSERT INTO `users` (`user_username`,`user_password`) VALUES ("+"'"+str(content['login'])+"','"+str(content['passw'])+"'"+")"
+    msg = "INSERT INTO `users` (`user_username`,`user_password`) VALUES ("+"'"+str(content['login'])+"','"+str(content['password'])+"'"+")"
     print msg
-#    msg = "INSERT INTO `users` (`user_username`,`user_password`) VALUES ('leszek','haslo')"
-#    print msg
     cursor.execute(msg)
     cursor.execute("SELECT * FROM users")
     rows = cursor.fetchall()
     for row in rows:
         print row
-    login = {content['login']:content['passw']}
+    login = {content['login']:content['password']}
     return json.dumps(login, indent=4)
 
 @app.route('/json', methods=['POST'])
@@ -56,12 +144,12 @@ def jsonTesting():
     conn = mysql.connection
     cursor = conn.cursor()
     login = content['login'] 
-    passw = content['passw']
+    password = content['password']
     ifLoginCorrect = 0
     cursor.execute("SELECT * FROM users")
     rows = cursor.fetchall()
     for row in rows:
-        if login == row[1] and passw == row[2]:
+        if login == row[1] and password == row[2]:
             ifLoginCorrect = 1
     if ifLoginCorrect == 1:
         return str(ifLoginCorrect)
@@ -70,24 +158,7 @@ def jsonTesting():
         return str(ifLoginCorrect)
 
 
-    #obj = [[1,2,3],(1,2,3),123,123.123,'abc',{'key1':(1,2,3),'key2':(4,5,6)}]
-    # Convert python object to json
-    #obj2 = {'login':'fikcyjny','passw':'fikcyjne'},{'login':'fikcyjny','passw':'fikcyjne'}
 
-    #json_string = json.dumps(obj)
-    #print 'Json: %s' % json_string
-    #abort(401)
-    #return json.dumps(obj2, indent=4)
-    #if request.method == "POST":
-#       zmienna = request.form['login']
-    #    content = request.json
-    #    print content['login']
-    #    return jsonify(login=content['login'],passw=content['passw'])
-#        return jsonify(login=request.form['login'],passw=request.form['passw'])  
-#    return 'blad'
-
-
-DATABASE = 'flask.json'
 
 class Main(flask.views.MethodView):
     def get(self):
@@ -97,13 +168,13 @@ class Main(flask.views.MethodView):
         if 'logout' in flask.request.form:
             flask.session.pop('username', None)
             return flask.redirect(flask.url_for('index'))
-        # required = ['username', 'passwd']
+        # required = ['username', 'password']
         # for r in required:
         #     if r not in flask.request.form:
         #         flask.flash("Error: {0} is required.".format(r))
         #         return flask.redirect(flask.url_for('index'))
         username = flask.request.form['username']
-        passwd = flask.request.form['passwd']
+        password = flask.request.form['password']
         ifLoginProperly = 0
         content = request.json
         conn = mysql.connection
@@ -112,7 +183,7 @@ class Main(flask.views.MethodView):
         rows = cursor.fetchall()
         for row in rows:
             print row
-            if username == row[1] and passwd == row[2]:
+            if username == row[1] and password == row[2]:
                 flask.session['username'] = username
                 ifLoginProperly = 1
         if ifLoginProperly == 0:
@@ -141,44 +212,65 @@ class Add(flask.views.MethodView):
     @login_required
     def post(self):
         if 'userList' in flask.request.form:
-            flask.flash('Niedlugo wyswietlanie listy uzytkownik bedzie dostepne')
+            listOfUsers = showUsersInDatabase()
+            #flask.flash(str(usersList))
+            loginList = []
+            for user in listOfUsers:
+                loginList.append(user[1])
+            for logins in loginList:
+		print logins 
+                flask.flash(logins+u'\r\n')
+            #flask.flash('Niedlugo wyswietlanie listy uzytkownik bedzie dostepne')
         content = request.json
         conn = mysql.connection
         cursor = conn.cursor()
-        cursor.execute("SELECT * FROM users")
-        username = flask.request.form['login']
-        passwd = flask.request.form['password']
-        print username, passwd
-        mysqlCmdAddUser = "INSERT INTO `users` (`user_username`,`user_password`) VALUES ("+"'"+username+"','"+passwd+"'"+")"
-        print mysqlCmdAddUser
-        cursor.execute(mysqlCmdAddUser)
-        rows = cursor.fetchall()
-        for row in rows:
-            print row
-        #    msg = "INSERT INTO `users` (`user_username`,`user_password`) VALUES ('leszek','haslo')"
-        # flask.flash("lista uzytkownikow:"+'\n')
-        # flask.flash(fr)
+	if 'addUser' in flask.request.form:
+            username = flask.request.form['login']
+            password = flask.request.form['password']
+            result = addUserToDatabase(username,password)
+            flask.flash(result)
+            print 'po wyjsciu z funkcji dodawania'
+            cursor.execute("SELECT * FROM users")
+	    rows = cursor.fetchall()
+	    for row in rows:
+	        print row
+	if 'removeUser' in flask.request.form:
+	    userToRemoved = flask.request.form['login_to_removed'] 
+	    print userToRemoved
+	    result = removeUserFromDatabase(userToRemoved)
+            flask.flash(result)
         return render_template('addUser.html')
 
-class UserStats(flask.views.MethodView):
-    @login_required
-    def get(self):
-        return flask.render_template('show_entries.html')
+#DATABASE="flask.json"
 
-    @login_required
-    def post(self):
-        try:
-            db = open(DATABASE).read()
-        except IOError:
-            db = '{"entries":[]}'
-        g.db = json.loads(db)
-        g.db['entries'].insert(0, {
-        'title': request.form['title'],
-        'text':request.form['text']
-        })
-        if hasattr(g,'db'):
-            open(DATABASE, 'w').write(json.dumps(g.db, indent=4))
-        return render_template('show_entries.html', entries=g.db['entries'])
+class ShowRaces(flask.views.MethodView):
+     @login_required
+     def get(self):
+         return flask.render_template('races.html')
+
+     @login_required
+     def post(self):
+         return render_template('races.html')
+
+#class UserStats(flask.views.MethodView):
+#    @login_required
+#    def get(self):
+#        return flask.render_template('show_entries.html')
+
+#    @login_required
+#    def post(self):
+#        try:
+#            db = open(DATABASE).read()
+#        except IOError:
+#            db = '{"entries":[]}'
+#        g.db = json.loads(db)
+#        g.db['entries'].insert(0, {
+#        'title': request.form['title'],
+#        'text':request.form['text']
+#        })
+#        if hasattr(g,'db'):
+#            open(DATABASE, 'w').write(json.dumps(g.db, indent=4))
+#        return render_template('show_entries.html', entries=g.db['entries'])
 
 class ShowResults(flask.views.MethodView):
     def get(self):
@@ -200,8 +292,8 @@ app.add_url_rule('/',
 app.add_url_rule('/add/',
                  view_func=Add.as_view('add'),
                  methods=['GET', 'POST',])
-app.add_url_rule('/stats/',
-                 view_func=UserStats.as_view('stats'),
+app.add_url_rule('/races/',
+                 view_func=ShowRaces.as_view('races'),
                  methods=['GET', 'POST'])
 app.add_url_rule('/results/',
                  view_func=ShowResults.as_view('results'),
